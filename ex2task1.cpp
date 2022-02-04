780a9412d9e7e07928d01703ff1d77bc30637481
NikitaHIP <98410239+NikitaHIP@users.noreply.github.com>
2022-02-04 04:34:00 +0200

    Add files via upload

---
+++ ex2task1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int integer;
+	
+	cout << "Enter integer ";
+	cin >> integer;
+	cout << "Hello World " << integer << endl;  
+	return 0;                        
+}
